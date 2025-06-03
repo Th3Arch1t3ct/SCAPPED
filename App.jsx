8c625ebb2d97f48998634a5d9ab62f0e921f7882
GR <gilbertojramiro@gmail.com>
2025-06-02 18:21:46 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Tourist from "./components/Tourist/Tourist";
+import Purist from "./components/Purist/purist";
+
+function RouteHandler() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const favicon = document.querySelector('link[rel="icon"]');
+    
+    if (location.pathname === '/den') {
+      favicon.href = '/GRsigned.png';
+      document.title = 'Gilberto Ramiro™';
+    } else {
+      favicon.href = '/blue.png';
+      document.title = 'GR™ ';
+    }
+  }, [location.pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Tourist />} />
+      <Route path="/Den" element={<Purist />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <RouteHandler />
+    </Router>
+  );
+}
+
+export default App;
